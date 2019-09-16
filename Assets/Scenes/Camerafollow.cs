c29224be6834db16b86b99a4f0fa2330c211bcf1
Anson <anson.liang@hotmail.com>
2019-09-16 12:59:15 -0600

    Demo

---
+++ Assets/Scenes/Camerafollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour
+
+{
+    public Transform target;
+    Vector3 carmera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carmera = target.position;
+        carmera.z = -10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        carmera = target.position;
+        carmera.z = -10;
+        transform.position = carmera;
+    }
+}
---
+++ Assets/Scenes/Motorctr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motorctr : MonoBehaviour
+{
+
+    public WheelJoint2D right;
+    public WheelJoint2D left;
+
+    public float speed = 1500;
+    public float rotate = 1500;
+
+    Rigidbody2D rg;
+
+
+    JointMotor2D motor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor.maxMotorTorque = 10000;
+        rg = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        motor.motorSpeed = speed * Input.GetAxis("Vertical");
+
+        right.motor = motor;
+        left.motor = motor;
+        rg.AddTorque(-rotate * Input.GetAxis("Horizontal") * Time.deltaTime);
+
+
+    }
+}
